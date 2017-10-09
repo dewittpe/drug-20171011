ea000ac34d6e95c7394cdc17fa3c4037979176c1
Peter DeWitt <dewittpe@gmail.com>
2017-10-09 14:55:33 -0600

    step 02: using enquo and notes about !! vs rlang::UQ

--- R/my_summary.R
+++ R/my_summary.R
@@ -16,7 +16,25 @@
 #'   stop("my_filter is broken", call. = FALSE)
 #' }
 #'
+#' v3 <- dplyr::filter(mtcars, mpg < 14 & hp > 220)
+#' v4 <- my_filter(mtcars, mpg < 14 & hp > 220)
+#' 
+#' if (!all.equal(v3, v4)) {
+#'   stop("my_filter is broken", call. = FALSE)
+#' }
+#'
 #' @export
 my_filter <- function(x, args) {
-  dplyr::filter(x, !!dplyr::enquo(args))
+  dplyr::filter(x, rlang::UQ(dplyr::enquo(args)))
 } 
+
+
+# NOTE: An equvialent function would use !!, a short hand for rlang::UQ.
+#       Read the vignette, it *might* be preferable to use UQ in packages as UQ
+#       is more robust than !!.
+# my_filter <- function(x, args) {
+#   dplyr::filter(x, !!(dplyr::enquo(args)))
+# } 
+
+# TODO:  What if you want to use an unknown number of arguments, i.e., use ...
+# instead of args?
